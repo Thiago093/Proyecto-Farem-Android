6328d44798d07e79b555e7428c6d12acb76371d2
Yesliani Carabali <yesliani@gmail.com>
2026-06-01 21:02:22 -0500

    Agrega pantallas Examen, Servicios y Acerca de FAREM

---
+++ app/src/main/java/com/example/proyectofarem/AcercaActivity.kt
@@ -0,0 +1,15 @@
+package com.example.proyectofarem
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AcercaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_acerca)
+    }
+}
